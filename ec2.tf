078490d249f542d05d55425024b933a5ea1a9ef5
Rajashekar A <rajeshekaraleti@gmail.com>
2025-05-28 22:56:43 +0530

    Terraform

---
+++ ec2.tf
@@ -0,0 +1,25 @@
+resource "aws_security_group" "allow_tls" {
+  name        = "allow_tls"
+  description = "Allow TLS inbound traffic and all outbound traffic"
+
+  tags = {
+    Name = "allow_tls"
+  }
+}
+
+
+
+
+
+
+
+
+
+# resource "aws_instance" "web" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "HelloWorld"
+  }
+#}
